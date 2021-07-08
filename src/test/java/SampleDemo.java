e3e18853178255b9b5d527a109a5f0c6e7c0950a
navitkumar03 <navitkumar03@gmail.com>
2021-07-08 13:40:15 +0530

    navit added new line

--- src/test/java/SampleDemo.java
+++ src/test/java/SampleDemo.java
@@ -2,5 +2,6 @@ public class SampleDemo {
     public static void main(String []rgs){
         System.out.println("hello first git project...");
         System.out.println("welcome new text");
+        System.out.println("added 3rd line to the main");
     }
 }

d802d1596bbf0901a6cbdebf19e5ceceda7c9e24
navitkumar03 <navitkumar03@gmail.com>
2021-07-08 13:15:39 +0530

    my 1st project code

---
+++ src/test/java/SampleDemo.java
@@ -0,0 +1,6 @@
+public class SampleDemo {
+    public static void main(String []rgs){
+        System.out.println("hello first git project...");
+        System.out.println("welcome new text");
+    }
+}
